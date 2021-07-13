3969b5ef7b8c2ef1c7a2c73a12791ce5e326cdaa
kbalev <kamen_balev@yahoo.com>
2021-07-13 14:51:51 +0100

    working properly

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const yargs = require("yargs")
 const command = process.argv[2];
 const fs = require("fs")
-const { add } = require("./utils")
-let movieList = [];
+const {
+    add,
+    remove,
+    update,
+    read
+} = require("./utils")
+exports.movieList = [];
 
 try {
     let tempJson = fs.readFileSync('./netflix.json');
@@ -11,5 +16,13 @@ try {
     movieList = [];
 }
 
-
-add()
+if (process.argv[2] == "add") {
+    add(movieList)
+} else if (process.argv[2] == "add") {
+    remove(movieList)
+} else if (process.argv[2] == "add") {
+    update(movieList)
+} else {
+    read(movieList)
+    console.log("This is your current list. If this is not the command you were looking for, someting went wrong. Please try again.")
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,9 +2,51 @@ const fs = require("fs")
 const yargs = require("yargs")
 const command = process.argv[2];
 
-exports.add = () =>{
-    if (command ==="add") {
-        const entry = { title: yargs.argv.movie, actor: yargs.argv.actor}
-        console.log(entry);
+exports.add = (movieList) => {
+    if (command === "add") {
+        const entry = {
+            title: yargs.argv.movie,
+            status: "unfinished"
+        };
+        movieList.push(entry);
+        console.log(movieList)
+        let stringMovieList = JSON.stringify(movieList)
+        fs.writeFileSync('./netflix.json', stringMovieList)
+        console.log(`You have added ${yargs.argv.movie} to your watchlist. Its default status is set to "Unfinished"`)
+    }
+}
+
+exports.remove = (movieList) => {
+    if (process.argv[2] === "remove") {
+        target = yargs.argv.movie
+        for (let i = 0; i < movieList.length; i++) {
+            if (target === movieList[i].title) {
+                movieList.splice([i], 1)
+                let stringMovieList = JSON.stringify(movieList)
+                fs.writeFileSync('./netflix.json', stringMovieList);
+                console.log(`You have removed ${yargs.argv.movie}`)
+            }
+        }
+    }
+}
+
+exports.update = (movieList) => {
+    if (process.argv[2] === "update") {
+        target = yargs.argv.title
+        newStatus = yargs.argv.status
+        for (let i = 0; i < movieList.length; i++) {
+            if (target == movieList[i].title) {
+                movieList[i].status = newStatus;
+                let stringMovieList = JSON.stringify(movieList);
+                fs.writeFileSync('./netflix.json', stringMovieList);
+                console.log(`You have set the status of ${yargs.argv.title} to "${yargs.argv.status}".`)
+            }
+        }
+    }
+}
+
+exports.read = (movieList) => {
+    if (process.argv[2] === "list") {
+        console.log(movieList);
     }
 }
